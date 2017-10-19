ee7132360bec96acf09dbb925ff3f97c5ff963d1
Patrick-Woo <sd4174263@163.com>
2017-10-19 11:52:21 +0800

    submission

---
+++ src/PID.cpp
@@ -0,0 +1,32 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+}
+
+void PID::UpdateError(double cte) {
+	// d_error is difference from old cte (p_error) to the new cte
+	d_error = (cte - p_error);
+	// p_error gets set to the new cte
+	p_error = cte;
+	// i_error is the sum of ctes to this point
+	i_error += cte;
+}
+
+double PID::TotalError() {
+    double u = -Kp * p_error - Kd * d_error - Ki * i_error;
+    return u;
+}
+
